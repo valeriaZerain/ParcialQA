0a453195627b06606df3ffd962e1829b69e51cf6
vale_ <valeria.zerain@gmail.com>
2024-10-07 17:41:42 -0400

    Examen commit 1

--- src/main/java/examen1/DateUtils.java
+++ src/main/java/examen1/DateUtils.java
@@ -1,2 +1,43 @@
-package examen1;public class DateUtils {
+package examen1;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateUtils {
+    public String getDayOfWeek(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        try {
+            formatter.parse(date);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Invalid date format. Expected format: yyyy-MM-dd");
+        }
+        String[] parts = date.split("-");
+        int a = Integer.parseInt(parts[0]);
+        int b = Integer.parseInt(parts[1]);
+        int c= Integer.parseInt(parts[2]);
+        boolean d = false;
+        if (b == 2 && c== 29)
+            d = true;
+        if (b == 1 && c== 1)
+            d = false;
+        if (b == 1 || b == 2) {
+            b += 12;
+            a -= 1;
+        }
+        int k = a % 100;
+        int j = a / 100;
+        int f = c+ (13 * (b + 1)) / 5 + k + (k / 4) + (j / 4) - 2 * j;
+        int dayOfWeek = d?0:(f % 7 + 7) % 7;
+
+        switch (dayOfWeek) {
+            case 0: return "SATURDAY";
+            case 1: return "SUNDAY";
+            case 2: return "MONDAY";
+            case 3: return "TUESDAY";
+            case 4: return "WEDNESDAY";
+            case 5: return "THURSDAY";
+            case 6: return "FRIDAY";
+            default: return "INVALID";
+        }
+    }
 }
--- src/test/java/examen1/DateTest.java
+++ src/test/java/examen1/DateTest.java
@@ -1,2 +1,30 @@
-package examen1;public class DateTest {
+package examen1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+//6.1) Se requiere crear los unit test necesarios para garantizar para el método getStringDate()
+//especificamente para el año 2024 retorna el dia en string basado en la fecha en el formato YYYY-MM-DD,
+//solo se deben crear los unit test,  el método bajo testeo se compartirá
+public class DateTest {
+    @Test
+    public void verifyDateIsCorrect(){
+        DateUtils date = new DateUtils();
+        String expectedResult = "MONDAY";
+        Assertions.assertEquals(expectedResult, date.getDayOfWeek("2024-10-07"));
+    }
+
+    @Test
+    public void verifyDateIsInvalid(){
+        DateUtils date = new DateUtils();
+        String expectedResult = "INVALID";
+        Assertions.assertEquals(expectedResult, date.getDayOfWeek("2024-02-30"));
+    }
+
+    @Test
+    public void verifyInvalidFormatOfDate(){
+        DateUtils date = new DateUtils();
+        String expectedResult = "MONDAY";
+        Assertions.assertEquals(expectedResult, date.getDayOfWeek("07-10-2024"), "Invalid date format. Expected format: yyyy-MM-dd");
+    }
 }
